cf89f0517dbbeeb2ca146d1737c051f561fe1a88
asfuyao <asfuyao@qq.com>
2021-04-30 19:12:02 +0800

    格式调整

--- createsm_mac.sh
+++ createsm_mac.sh
@@ -4,4 +4,6 @@ echo "Create gitbook summary"
 
 book sm -c ['MacOS','Linux','Windows','开发工具','开发技术','Git','数据库','虚拟化','硬件']
 
-sed -i '2a\- [说明](README.md)' SUMMARY.md
+sed -i '' "2a\\
+- [说明](README.md) \\
+" SUMMARY.md
