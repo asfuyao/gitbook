aa12a05815d9dc2be35772027c8d542587ff8798
asfuyao <asfuyao@qq.com>
2021-04-30 19:19:21 +0800

    格式调整

--- createsm.cmd
+++ createsm.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-book sm -c ['MacOS','Linux','Windows','开发工具','开发技术','Git','数据库','虚拟化','硬件']
+book sm -c ['Android','MacOS','Linux','Windows','开发工具','开发技术','Git','数据库','虚拟化','硬件']
 
 set sed="C:\Program Files\Git\usr\bin\sed"
 %sed% -i 2a\"- [说明](README.md)" SUMMARY.md
